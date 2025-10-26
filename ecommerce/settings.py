17e9ad4dd88930c2bc953dacb0829a9bc97e1970
Kona <konapromise81@gmail.com>
2025-10-26 15:47:23 -0700

    Fix Cloudinary environment variable names

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,12 +123,21 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # ======================================
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('doxicw3ap'),
-    'API_KEY': os.environ.get('121411195668158'),
-    'API_SECRET': os.environ.get('**********'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.environ.get('doxicw3ap'),
+#     'API_KEY': os.environ.get('121411195668158'),
+#     'API_SECRET': os.environ.get('**********'),
+# }
+
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
